8bea2612f6c1c542f63564177ecc1ddcfdcbba67
Vitória <vitcs11@gmail.com>
2023-04-16 17:06:55 -0300

    Requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false,
+      tableName: 'categories',
+      underscored: true,
+    },
+  );
+
+  return Category;
+};
